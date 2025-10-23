0ec93ad840a1674c84dcc354132d65efd800844e
Will D <will.kof1@gmail.com>
2025-10-22 23:16:35 -0300

    add test redis

---
+++ test_redis/var.tf
@@ -0,0 +1,47 @@
+variable "aws_region" {
+  description = "A região da AWS onde os recursos serão criados."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "Nome base para todos os recursos criados (cluster, SGs, etc.)."
+  type        = string
+  default     = "cluster-reshard-test"
+}
+
+variable "instance_type" {
+  description = "Tipo da instância EC2 que executará o script de teste."
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "redis_node_type" {
+  description = "Tipo de nó para o cluster ElastiCache Redis."
+  type        = string
+  default     = "cache.t3.small"
+}
+
+variable "redis_auth_token" {
+  description = "Authentication token for Redis"
+  type        = string
+  sensitive   = true
+}
+
+variable "vpc_id" {
+  description = "O ID da sua VPC existente."
+  type        = string
+  default     = "vpc-00ebbf529e98939d9" # SUBSTITUA PELO SEU VPC ID
+}
+
+variable "subnet_ids" {
+  description = "Uma lista de IDs de subnet existentes onde o cluster ElastiCache será criado."
+  type        = list(string)
+  default     = ["subnet-03ab19be2d59fd559", "subnet-06060f7cd8b84c789"]
+}
+
+variable "key_pair_name" {
+  description = "Nome do seu Key Pair da EC2 para acesso SSH. DEVE existir na região."
+  type        = string
+  default = "aws_will_prd"
+}
